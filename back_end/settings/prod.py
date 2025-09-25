8f9ef9245e63f94c5f50dc24cc061d43b0170527
Seniv Volodymyr <seniv.05@gmail.com>
2025-09-25 17:46:40 +0200

    fixed allowed hosts on prod

--- back_end/settings/prod.py
+++ back_end/settings/prod.py
@@ -3,7 +3,11 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = [
+    "task-manager-seniv-volodymyr.onrender.com",  # your Render domain
+    "localhost",
+    "127.0.0.1",
+]
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
